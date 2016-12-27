19f4d08b06c760c3a793166882f995b255fcfab8
liangbx <958800@gitlab.localhost>
2016-12-27 19:16:00 +0800

    添加bottom bar

--- easy-home-doctor-app/src/main/java/com/easy/home/doctor/android/MainActivity.java
+++ easy-home-doctor-app/src/main/java/com/easy/home/doctor/android/MainActivity.java
@@ -1,13 +1,29 @@
 package com.easy.home.doctor.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.roughike.bottombar.BottomBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_nav)
+    BottomBar mBottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        mBottomNav.setOnTabSelectListener((tabId) -> {
+            switch (tabId) {
+//                case R.id.nav
+            }
+        });
     }
 }
